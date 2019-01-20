3e21c65ce4c9ae9266988f57ae4b7fa99e745315
CameronRW <cameronrw@gmail.com>
2019-01-20 11:54:40 +0000

    App and Carousel mobile styles

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './Carousel.scss';
+import { ReactComponent as Arrow } from './arrow.svg';
 
 const Carousel = ({ images }) => (
     <article className="Carousel">
-        <div className="Carousel__wrapper">
-            <ul className="Carousel__container">
-                {Object.keys(images).map(key => (
-                    <li className="Carousel__item" key={key}>
+        <ul className="Carousel__container">
+            {Object.keys(images).map(key => (
+                <li className="Carousel__item" key={key}>
+                    <div className="Media">
                         <img src={images[key].webformatURL} alt={`${parseInt(key, 10) + 1} title`} />
-                        <p>{`Image ${parseInt(key, 10) + 1} title`}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+                    </div>
+                    <p>{`Image ${parseInt(key, 10) + 1} title`}</p>
+                </li>
+            ))}
+        </ul>
         <nav className="Carousel__nav">
             <button className="Carousel__button Carousel__button--prev" type="submit">
+                <Arrow />
                 <span>Prev</span>
             </button>
             <button className="Carousel__button Carousel__button--next" type="submit">
+                <Arrow />
                 <span>Next</span>
             </button>
         </nav>
